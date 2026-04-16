6cd737896ff7bce08b6c183682c3a5813e6d773c
fudiyangjin <578306088@qq.com>
2026-04-16 18:46:09 +0800

    feat: 修改为本机直连

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import Icons from 'unplugin-icons/vite'
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const loopbackHost = "127.0.0.1";
 
 // https://vitejs.dev/config/
 // @ts-ignore
@@ -28,9 +27,9 @@ export default defineConfig(async () => ({
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
-    strictPort: false,
-    host: host || false,
-    hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
+    strictPort: true,
+    host: loopbackHost,
+    hmr: { protocol: "ws", host: loopbackHost, port: 1421 },
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
